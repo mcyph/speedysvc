cda1412d00f14c6ba41e3e3237e1bb3164f9aaff
David Morrissey <20507948+mcyph@users.noreply.github.com>
2018-02-03 12:24:56 +1100

    started reorganising files in a big way, separating different modules into smaller setuptools packages with the goal of making things more manageable

---
+++ network_tools/mmap_sockets/Base.py
@@ -0,0 +1,57 @@
+import struct
+
+from ctypes import c_ushort, c_uint
+
+
+class Base:
+    # mmap path for transfers
+    PATH = '/tmp/mmaptest-%s'
+
+
+    # Number of threads on a server, and how
+    # many clients can connect at once
+    MAX_CONNECTIONS = 25
+
+
+    # time.sleep performance-related options
+    SLEEP_EVERY = 1000
+    SLEEP_DIV_BY = 10
+
+
+    # Current network state
+    STATE_DATA_TO_CLIENT = 0
+    STATE_CMD_TO_SERVER = 1
+
+
+    SClientValues = {
+        STATE_DATA_TO_CLIENT
+    }
+
+
+    # Remote commands
+    CMD_GET = 0
+    CMD_PUT = 1
+    CMD_ITER_STARTSWITH = 2
+
+
+
+    def get_variables(self, buf):
+        cur_state_int = (
+            c_ushort.from_buffer(buf)
+        )
+        AMOUNT_OFFSET = (
+            struct.calcsize('@'+cur_state_int._type_)
+        )
+
+        amount_int = c_uint.from_buffer(buf, AMOUNT_OFFSET)
+        DATA_OFFSET = AMOUNT_OFFSET + struct.calcsize(
+            '@'+amount_int._type_
+        )
+
+        return (
+            cur_state_int,
+            amount_int,
+            DATA_OFFSET
+        )
+
+
---
+++ network_tools/mmap_sockets/Server.py
@@ -0,0 +1,99 @@
+import os
+import mmap
+import time
+import thread
+
+from Base import Base
+
+
+class Server(Base):
+    def __init__(self, DCmds):
+        self.DCmds = DCmds
+
+        self.num_threads = 1
+        thread.start_new_thread(self.main, (0,))
+
+
+    def main(self, thread_num):
+        print 'Starting new server thread:', thread_num
+
+        # Open the file, and zero out the data
+        path = self.PATH % thread_num
+        fd = os.open(path, os.O_CREAT|os.O_TRUNC|os.O_RDWR)
+        sz = 10485760 # 10MB
+        sz -= sz % mmap.PAGESIZE
+        assert os.write(fd, '\x00' * sz) == sz
+
+        buf = mmap.mmap(
+            fd, sz,
+            mmap.MAP_SHARED,
+            mmap.PROT_WRITE|mmap.PROT_READ
+        )
+        cur_state_int, amount_int, DATA_OFFSET = (
+            Base.get_variables(self, buf)
+        )
+
+        # Assign some communication values locally
+        t = time.time()
+
+
+
+        while 1:
+            if cur_state_int.value == self.STATE_CMD_TO_SERVER:
+
+                if (
+                    thread_num+1 == self.num_threads and
+                    self.num_threads+1 < self.MAX_CONNECTIONS
+                ):
+                    self.num_threads += 1
+                    thread.start_new_thread(self.main, (self.num_threads-1,))
+
+
+                # Get the command/command argument from the client
+                recv_data = buf[DATA_OFFSET:DATA_OFFSET+amount_int.value]
+                cmd, _, recv_data = recv_data.partition(' ')
+
+                # Send a response to the client
+                send_data = self.DCmds[cmd](recv_data)
+                #assert isinstance(send_data, str), (cmd, repr(send_data))
+
+
+
+                # Resize the mmap if data is too large for it
+                if len(send_data) > (sz-DATA_OFFSET):
+                    sz = int(len(send_data) * 1.5)
+                    sz -= sz % mmap.PAGESIZE
+                    print 'RESIZE MMAP:', sz, len(send_data)
+                    buf.resize(sz)
+
+                    # These variables are c pointers which
+                    # become invalid after a resize
+                    cur_state_int, amount_int, DATA_OFFSET = (
+                        Base.get_variables(self, buf)
+                    )
+
+
+                buf[DATA_OFFSET:DATA_OFFSET+len(send_data)] = send_data
+                amount_int.value = len(send_data)
+                # This might be overkill, but just to be sure...
+                assert buf[DATA_OFFSET:DATA_OFFSET+len(send_data)] == send_data
+                assert amount_int.value == len(send_data)
+
+                cur_state_int.value = self.STATE_DATA_TO_CLIENT
+                t = time.time()
+
+
+            i_t = time.time()-t
+            if i_t > 1:
+                time.sleep(0.05)
+            elif i_t > 0.1:
+                time.sleep(0.001)
+
+
+if __name__ == '__main__':
+    inst = Server({
+        'echo': lambda data: data
+    })
+
+    while 1:
+        time.sleep(1)
