ed82d53f6e148c7161f14f5b31ada76353655e1a
David Morrissey <20507948+mcyph@users.noreply.github.com>
2019-06-16 23:13:11 +1000

    cleaned up setup.py

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+PYTHON_IMPLEMENTATION="$(python -c 'import platform; print(platform.python_implementation())')"
+rm -rf build/
+tr '\n' '\0' < uninstall_files_${PYTHON_IMPLEMENTATION}.txt | xargs -0 rm -f --
+python2 setup.py install --force --record uninstall_files_${PYTHON_IMPLEMENTATION}.txt
