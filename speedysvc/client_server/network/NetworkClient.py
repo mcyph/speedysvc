dc2d3fd9b435423136e80635fb984d4fe0dca637
Dave Morrissey <20507948+mcyph@users.noreply.github.com>
2020-03-08 18:07:23 +1100

    added auto-reconnect logic to NetworkClient.py

--- speedysvc/client_server/network/NetworkClient.py
+++ speedysvc/client_server/network/NetworkClient.py
@@ -1,7 +1,7 @@
 import time
-import snappy
+import warnings
 import socket
-from struct import Struct
+from os import getpid
 from speedysvc.toolkit.documentation.copydoc import copydoc
 
 from speedysvc.client_server.base_classes.ClientProviderBase import ClientProviderBase
@@ -19,18 +19,28 @@ class NetworkClient(ClientProviderBase):
         :param server_methods:
         :param host:
         """
+        self.host = host
+        self.port = port
+        ClientProviderBase.__init__(self, server_methods)
+        self.compression_inst = compression_inst
+        self.__connect()
+
+    def __connect(self):
         self.conn_to_server = conn_to_server = socket.socket(
             socket.AF_INET, socket.SOCK_STREAM
         )
         conn_to_server.setsockopt(
             socket.SOL_TCP, socket.TCP_NODELAY, 1
         )
-        port = port if port is not None else server_methods.port
-        conn_to_server.connect((host, port))
-        conn_to_server.send(compression_inst.typecode)
-
-        ClientProviderBase.__init__(self, server_methods)
-        self.compression_inst = compression_inst
+        port = (
+            self.port
+            if self.port is not None
+            else self.server_methods.port
+        )
+        conn_to_server.connect((self.host, port))
+        conn_to_server.send(
+            self.compression_inst.typecode
+        )
 
     def __del__(self):
         self.conn_to_server.close()
@@ -41,7 +51,27 @@ class NetworkClient(ClientProviderBase):
             self.compression_inst.compress(fn.serialiser.dumps(data))
         cmd = fn.__name__.encode('ascii')
         prefix = len_packer.pack(int(actually_compressed), len(data), len(cmd))
-        self.conn_to_server.send(prefix + cmd + data)
+
+        displayed_reconnect_msg = False
+        while True:
+            # Try to keep reconnecting if
+            # connection no longer functioning
+            try:
+                self.conn_to_server.send(prefix + cmd + data)
+                break
+            except socket.error:
+                if not displayed_reconnect_msg:
+                    displayed_reconnect_msg = True
+                    warnings.warn(
+                        f"Client [pid {getpid()}]: "
+                        f"TCP connection to service "
+                        f"{self.server_methods.name} reset - "
+                        f"the service may need to be checked/restarted!"
+                    )
+
+                import time
+                time.sleep(1)
+                self.__connect()
 
         def recv(amount):
             # Note string concatenation is slower in earlier versions
