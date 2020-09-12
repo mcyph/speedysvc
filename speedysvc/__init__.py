ae1c1336dbde6b81e42334b9480e76b6bd71e307
Dave Morrissey <20507948+mcyph@users.noreply.github.com>
2020-09-12 21:05:34 +1000

    added various module exports to __init__.py

--- speedysvc/__init__.py
+++ speedysvc/__init__.py
@@ -1,4 +1,3 @@
-"""
 from hybrid_lock import HybridLock
 from hybrid_lock import \
     CONNECT_OR_CREATE, CONNECT_TO_EXISTING, \
@@ -10,9 +9,8 @@ from speedysvc.client_server.network.NetworkServer import NetworkServer
 from speedysvc.rpc_decorators import \
     json_method, marshal_method, msgpack_method, \
     raw_method, pickle_method
-from speedysvc.logger.std_logging.LoggerServer import LoggerServer
-from speedysvc.logger.std_logging.LoggerClient import LoggerClient
-from speedysvc.logger.time_series_data.ServiceTimeSeriesData import \
-    ServiceTimeSeriesData
+#from speedysvc.logger.std_logging.LoggerServer import LoggerServer
+#from speedysvc.logger.std_logging.LoggerClient import LoggerClient
+#from speedysvc.logger.time_series_data.ServiceTimeSeriesData import \
+#    ServiceTimeSeriesData
 from speedysvc.client_server.connect import connect
-"""
